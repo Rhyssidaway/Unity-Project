a6334edb1f69df634d5b9073bdc968d6372c5f4a
Rhyssidaway <30109610@Tafe.wa.edu.au>
2026-02-10 14:41:58 +0800

    Update Operators.cs

--- Unity project/Assets/Scripts/Operators.cs
+++ Unity project/Assets/Scripts/Operators.cs
@@ -11,20 +11,25 @@ using UnityEngine;
 
 public class Operators : MonoBehaviour
 {
+    [Header("Variables")] // Creates a header in inspector
     public int health = 100;
     public int dmgValue = 10;
-    private int medpack = 5;
 
-    
+    private int bonusVal = 10;
+    private int bonusMult = 2;
+    private int newBonus;
+
 
     void Start()
     {
+        newBonus = (bonusVal * bonusMult);
         Debug.Log("Health: " + health);
-        Debug.Log("Damage: " + dmgValue);
-        Debug.Log("Medpack: " + medpack);
+        Debug.Log("Bonus: " + bonusVal);
+        Debug.Log("Bonus multiplier: " + bonusMult);
+        Debug.Log("Current Bonus: " + newBonus);
+        Debug.Log("New health: " + (health + newBonus));
 
-        health -= dmgValue;
-
-        Debug.Log("Health: " + health);
+        Debug.Log(10 + 5 * (20 / 4) - 3);
     }
 }
+ 
