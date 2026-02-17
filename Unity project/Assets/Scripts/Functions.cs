ca8a44a6577d0e0e2f3eb136e50e47d188cc33ad
Rhyssidaway <30109610@Tafe.wa.edu.au>
2026-02-17 11:55:40 +0800

    Added basic arithmetic functions

--- Unity project/Assets/Scripts/Functions.cs
+++ Unity project/Assets/Scripts/Functions.cs
@@ -4,6 +4,7 @@ public class Functions : MonoBehaviour
     public int myHealth = 100;
     public int myDamage = 10;
     private float random;
+    private int newNumber;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -22,6 +23,17 @@ public class Functions : MonoBehaviour
         DisplayHealth(); // 120
 
         Debug.Log("Random Number: " + random);
+
+        SayHello();
+
+        newNumber = GiveMeANumber(); // Assigns 100 to newNumber
+
+        newNumber = Add(10, 40); // Assigns 10 + 40
+        Debug.Log("10 + 40 = " + newNumber);
+
+        newNumber = Subtract(newNumber, 10); // Subtracts 10 from 50
+        Debug.Log("50 - 10 = " + newNumber);
+
     }
 
     // Update is called once per frame
@@ -39,5 +51,21 @@ public class Functions : MonoBehaviour
     {
         return Random.value;
     }
-    
+
+    public void SayHello() // 'Says Hello!'
+    {
+        Debug.Log("Hello!");
+    }
+    public int GiveMeANumber() // Returns 100 as integer
+    {
+        return 20 + 80;
+    }
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+    public int Subtract(int a, int b)
+    {
+        return a - b;
+    }
 }
