22c246aa347e0c217967896c6ed39a162f632f86
Rhyssidaway <30109610@Tafe.wa.edu.au>
2026-02-24 13:23:43 +0800

    Added interation action
    - interacting with a cube launches it
    - added many cubes for more fun

--- Unity project/Assets/Scripts/Interact.cs
+++ Unity project/Assets/Scripts/Interact.cs
@@ -5,9 +5,13 @@ public class Interact : MonoBehaviour
 {
     public Transform player;
     public Transform cube;
+    public Rigidbody cubeRb;
 
+    public int distanceNeeded;
     public GameObject interactUI;
     public float playerDistance;
+    public float jumpForce = 1f;
+    public float pushForce = 1f;
     private bool ePressed = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,7 +23,8 @@ public class Interact : MonoBehaviour
     void Update()
     {
         playerDistance = (Mathf.Sqrt(Mathf.Pow((player.transform.position.x - cube.transform.position.x), 2) + Mathf.Pow((player.transform.position.y - cube.transform.position.y), 2) + Mathf.Pow((player.transform.position.z - cube.transform.position.z), 2))); // sqrt((x2 - x1)^2 + (y2 - y1)^2 + (z2 - z1)^2) // axis2 = player pos axis1 = cube pos
-        if (playerDistance < 2)
+        interactUI.SetActive(false);
+        if (playerDistance < distanceNeeded)
         {
             interactUI.SetActive(true);
             if (Input.GetKeyDown("e"))
@@ -28,17 +33,14 @@ public class Interact : MonoBehaviour
                 if (ePressed)
                 {
                     Debug.Log("E Pressed");
+                    cubeRb.AddForce(Vector3.up * jumpForce * 10, ForceMode.Impulse);
+                    cubeRb.AddForce(Vector3.forward * pushForce * 10, ForceMode.Impulse);
                 }
             }
             else
             {
                 ePressed = false;
             }
-            
-        }
-        else
-        {
-            interactUI.SetActive(false);
         }
     }
 }
