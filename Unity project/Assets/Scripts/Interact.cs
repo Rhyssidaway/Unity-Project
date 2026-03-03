ee7ddd77ca51538c00d482eb12e0108d6d800e69
Rhyssidaway <30109610@Tafe.wa.edu.au>
2026-03-03 09:50:18 +0800

    Interaction Fixes
    Fixed cube launching in only one direction - cube now launches in the direction the player is facing.

--- Unity project/Assets/Scripts/Interact.cs
+++ Unity project/Assets/Scripts/Interact.cs
@@ -13,17 +13,16 @@ public class Interact : MonoBehaviour
     public float jumpForce = 1f;
     public float pushForce = 1f;
     private bool ePressed = false;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        playerDistance = (Mathf.Sqrt(Mathf.Pow((player.transform.position.x - cube.transform.position.x), 2) + Mathf.Pow((player.transform.position.y - cube.transform.position.y), 2) + Mathf.Pow((player.transform.position.z - cube.transform.position.z), 2))); // sqrt((x2 - x1)^2 + (y2 - y1)^2 + (z2 - z1)^2) // axis2 = player pos axis1 = cube pos
         interactUI.SetActive(false);
+        playerDistance = Vector3.Distance(player.position, cube.position);
         if (playerDistance < distanceNeeded)
         {
             interactUI.SetActive(true);
@@ -34,7 +33,7 @@ public class Interact : MonoBehaviour
                 {
                     Debug.Log("E Pressed");
                     cubeRb.AddForce(Vector3.up * jumpForce * 10, ForceMode.Impulse);
-                    cubeRb.AddForce(Vector3.forward * pushForce * 10, ForceMode.Impulse);
+                    cubeRb.AddForce(player.forward * pushForce * 10, ForceMode.Impulse);
                 }
             }
             else
