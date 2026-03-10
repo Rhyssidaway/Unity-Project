6ed4acf88bd933a8db3695f2cd7e92021b300538
Rhyssidaway <30109610@Tafe.wa.edu.au>
2026-03-10 14:34:10 +0800

    .

--- Unity project/Assets/Scripts/DataStructures.cs
+++ Unity project/Assets/Scripts/DataStructures.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DataStructures : MonoBehaviour
@@ -7,6 +8,7 @@ public class DataStructures : MonoBehaviour
     //public int[] scores = new int[20];
 
     public int[,] table = new int[5, 5];
+    Dictionary<string, int> inventory = new Dictionary<string, int>();
 
     void Start()
     {
@@ -19,17 +21,20 @@ public class DataStructures : MonoBehaviour
         //    Debug.Log(i);
         //}
 
-        for(int c=0; c<=4; c++)
-        {
-            for (int r = 0; r <= 4; r++)
-            {
-                table[c, r] = (c+1) * (r+1);
-            }
-        }
-        foreach (int cell in table)
-        {
-            Debug.Log(cell);
-        }
+        //for(int c=0; c<=4; c++)
+        //{
+        //    for (int r = 0; r <= 4; r++)
+        //    {
+        //        table[c, r] = (c+1) * (r+1);
+        //    }
+        //}
+        //foreach (int cell in table)
+        //{
+        //    Debug.Log(cell);
+        //}
+
+        inventory["Swords"] = 1;
+        
     }
 
     void Update()
