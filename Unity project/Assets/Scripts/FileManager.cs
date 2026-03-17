c352ad5888431b752f6c1ff15e6334d387c1c857
Rhyssidaway <30109610@Tafe.wa.edu.au>
2026-03-17 12:04:42 +0800

    Added File manager script
    - Checks for directory existence
    - Creates directory if not
    - Adds the filename to the file
    
    **Still working on this**

---
+++ Unity project/Assets/Scripts/FileManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.IO;
+
+public class FileManager : MonoBehaviour
+{
+    public string textFileName;                  // This is the file name
+    public string[] textFileContents;            // This array will store the file contents
+
+    void Start()
+    {
+        
+    }
+
+    public void CreateNewTextFile(string textFileName)
+    {
+        // Create a file path string for the full path on that specific computer
+        string directoryPath = Application.dataPath + "/DataFiles" + textFileName + ".txt";
+
+        // Check if the path exists
+        // If not then create the folder
+        if (File.Exists(directoryPath) == false)
+        {
+            // Doesnt exist so create it
+            Directory.CreateDirectory(Application.dataPath + "/DataFiles");
+
+            // Create the file with the name of the file as the first line
+            File.WriteAllText(directoryPath, textFileName + '\n');
+        }
+    }
+
+}
